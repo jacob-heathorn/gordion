ca0af43f79e4f441ed7fa10bf47d72cac76cc9e5
Jacob <jacob.heathorn@gmail.com>
2024-05-06 14:03:12 +0700

    Verify existing repo

--- test/test_repository.py
+++ test/test_repository.py
@@ -4,15 +4,21 @@ from gordion.repository import Repository
 assert 'TOXTEMPDIR' in os.environ, "you must run these tests using tox"
 
 REPOS_DIR = os.path.join(os.environ['TOXTEMPDIR'], 'repos')
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def test_is_git_repository():
 
-  path = os.path.join(REPOS_DIR, 'west_example_a')
   url = 'dontcare'
   tag = 'dontcare'
   branch = 'dontcare'
 
   # Repository does not exist yet
+  path = os.path.join(REPOS_DIR, 'west_example_a')
   repo = Repository(path, url, tag, branch)
   assert not repo._is_git_repository()
+
+  # Check that self is a repository
+  path = os.path.join(SCRIPT_DIR)
+  repo = Repository(path, url, tag, branch)
+  assert repo._is_git_repository()
