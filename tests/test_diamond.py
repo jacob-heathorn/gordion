8dd93cfe06e579b8418eb0862e41d52e3716ccdf
Jacob <jacob.heathorn@gmail.com>
2024-07-17 13:06:29 +0800

    Move session to conftest.py

--- tests/test_diamond.py
+++ tests/test_diamond.py
@@ -2,27 +2,6 @@ import os
 import gordion
 import pytest
 
-assert 'TOXTEMPDIR' in os.environ, "you must run these tests using tox"
-
-REPOS_DIR = os.path.join(os.environ['TOXTEMPDIR'], 'repos')
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-
-
-@pytest.fixture(scope="session")
-def repo_a_session():
-  """
-  Creates the repo_a object only once for the lifetime of this session. This is important so the
-  "fetch_once" doesn't fetch every test case, which saves time.
-  """
-  path = os.path.join(REPOS_DIR, 'gordion_demo_a')
-  url = 'https://github.com/jacob-heathorn/gordion_demo_a.git'
-
-  # Create the repo object and ensure it.
-  repo = gordion.Repository(path)
-  repo.ensure(url)
-
-  yield repo
-
 
 @pytest.fixture
 def repo_a(repo_a_session):
@@ -52,7 +31,6 @@ def repo_a(repo_a_session):
   # Git clean.
   def cleanup_repo(path):
     if os.path.exists(path):
-      print(f"Git clean {path}")
       repo = gordion.Repository(path)
       repo.ensure()
       repo.handle.git.reset('--hard')
--- tests/test_single.py
+++ tests/test_single.py
@@ -3,9 +3,7 @@ import gordion
 import pytest
 from git import BadName
 
-assert 'TOXTEMPDIR' in os.environ, "you must run these tests using tox"
 
-REPOS_DIR = os.path.join(os.environ['TOXTEMPDIR'], 'repos')
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -17,22 +15,6 @@ def test_exists():
   # Verify this repository root is a git repository path.
   path = os.path.join(SCRIPT_DIR, '..')
   assert gordion.Repository._exists(path)
-
-
-@pytest.fixture(scope="session")
-def repo_a_session():
-  """
-  Creates the repo_a object only once for the lifetime of this session. This is important so the
-  "fetch_once" doesn't fetch every test case, which saves time.
-  """
-  path = os.path.join(REPOS_DIR, 'gordion_demo_a')
-  url = 'https://github.com/jacob-heathorn/gordion_demo_a.git'
-
-  # Create the repo object and ensure it.
-  repo = gordion.Repository(path)
-  repo.ensure(url)
-
-  yield repo
 
 
 @pytest.fixture
