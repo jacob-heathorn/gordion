e0a1d77fe8e71b9896421676cc7b8c7d6468c35d
Jacob <jacob.heathorn@gmail.com>
2024-07-30 12:39:38 +0800

    Fix error format

--- src/gordion/exception.py
+++ src/gordion/exception.py
@@ -13,22 +13,22 @@ class UpdateError(Exception):
 class UpdateLocalBranchAheadError(UpdateError):
   def __init__(self, target, local_branch, remote_branch, num_commits):
     reason = (f"{local_branch} is {num_commits} commit(s) ahead of {remote_branch}. "
-              f"Update would lose one or more of these commit(s).")
-    suggestion = "Push or remove the commits please."
+              f"Update would lose one or more of these commit(s)")
+    suggestion = "Push or remove the commits please"
     super().__init__(target, reason, suggestion)
 
 
 class UpdateNoTrackingBranchError(UpdateError):
   def __init__(self, target, local_branch):
-    reason = f"{local_branch} does not have a tracking branch, so commits will be lost."
+    reason = f"{local_branch} does not have a tracking branch, so commits will be lost"
     suggestion = f"Create a remote tracking branch (git push -u origin {local_branch})"
     super().__init__(target, reason, suggestion)
 
 
 class UpdateWrongTrackingBranchError(UpdateError):
   def __init__(self, target, local_branch, remote_branch):
-    reason = (f"{local_branch} does not track origin/{local_branch}. Instead, it tracks\n\t\t"
-              f"{remote_branch}. This is unexpected and can cause problems.")
+    reason = (f"{local_branch} does not track origin/{local_branch}. Instead, it tracks "
+              f"{remote_branch}. This is unexpected and can cause problems")
     suggestion = f"fix it: git push -u origin {local_branch}. Maybe delete incorrect remote branch?"
     super().__init__(target, reason, suggestion)
 
@@ -45,31 +45,26 @@ class UpdateDetachedHeadNotSavedError(UpdateError):
 
 class UpdateRepoIsDirtyError(UpdateError):
   def __init__(self, target):
-    reason = ("The repository is dirty and you are trying to move the HEAD commit.")
-    suggestion = "Save or restore the uncommitted changes."
+    reason = ("The repository is dirty and you are trying to move the HEAD commit")
+    suggestion = "Save or restore the uncommitted changes"
     super().__init__(target, reason, suggestion)
 
 
 class UpdateDuplicateRepoPathError(UpdateError):
   def __init__(self, target, other):
-    tab = " " * 8
-    reason = (f"The repository({other.url}) is already cloned at\n{tab}"
-              f"{other.path}.\n{tab}"
-              f"You are trying to clone it again to\n{tab}"
-              f"{target.path}.\n{tab}"
-              "You cannot do this.")
-    tab = " " * 12
-    suggestion = (f"You need to make sure all listings of the same repository have the same\n{tab}"
+    reason = (f"The repository({other.url}) is already cloned at {other.path}. You are trying to "
+              f"clone it again to {target.path}. You cannot do this.")
+    suggestion = ("You need to make sure all listings of the same repository have the same "
                   "local path in the gordion.yaml file.")
     super().__init__(target, reason, suggestion)
 
 
 class UpdateDuplicateRepoTagError(UpdateError):
   def __init__(self, target, target_tag, other, other_tag):
-    reason = (f"Gordion repository tag mismatch!\n\t"
-              f"{target._listed_path()}:{target_tag}\n\t"
-              f"{other._listed_path()}:{other_tag}")
-    suggestion = ("The tags need to match. I guess that's kinda the whole point of this thing.")
+    reason = (f"Gordion repository tag mismatch!\n"
+              f"{target._listed_path()}:{target_tag}\n"
+              f"{other._listed_path()}:{other_tag}\n")
+    suggestion = ("The tags need to match. I guess that's kinda the whole point of this thing")
     super().__init__(target, reason, suggestion)
 
 
