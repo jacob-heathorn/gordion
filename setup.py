5922a05ae88357b827468202351f2aa045e0525f
Jacob <jacob.heathorn@gmail.com>
2024-08-20 12:29:34 +0800

    Fix lint issues with gitpython

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
     ],
     install_requires=[
         'setuptools',
+        #'gitpython==3.1.43'
     ],
     python_requires='>=3.8',
     entry_points={'console_scripts': ('gordion = gordion.app.main:main',)},
