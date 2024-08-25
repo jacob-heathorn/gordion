613ba88d61b00833a03f57f0914476ee741ac9db
Jacob <jacob.heathorn@gmail.com>
2024-08-24 23:46:32 -0700

    test NOMINAL_STATUS

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,7 +1,17 @@
 import os
 import gordion
+from gordion.utils import green, bold_green, bold_blue
 import pytest
 from tests.conftest import recursive_git_blast
+
+
+NOMINAL_STATUS = f"""{bold_green('gordion_demo_a')} {green('test_status')}:{green('7e869f8')}
+└──{bold_blue('gordion')}
+    ├──{bold_green('gordion_demo_d')} {green('develop')}:{green('1e58b64')}
+    ├──{bold_blue('level_1')}
+    │   └──{bold_green('gordion_demo_b')} {green('develop')}:{green('64d65c2')}
+    └──{bold_blue('level_2')}
+        └──{bold_green('gordion_demo_c')} {green('develop')}:{green('ef7aabb')}"""
 
 
 @pytest.fixture
@@ -27,13 +37,13 @@ def demo_a(tree_a):
   tree_a.update(tag, branch_name, force=True)
 
 
-def test_gordion_status(demo_a):
+def test_nominal_status(demo_a):
   """
-  Verifies the gordion -s behavior.
+  Verifies the gordion -s behavior for the nominal test case (all green).
   """
 
   # From top of repoA.
   with gordion.utils.pushd(demo_a.path):
     root_path = gordion.app.root.gordion_root(os.getcwd())
     root = gordion.Tree(root_path)
-    print(gordion.app.status.get_status(root))
+    assert NOMINAL_STATUS == gordion.app.status.get_status(root)
